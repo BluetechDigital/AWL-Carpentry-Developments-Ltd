4ad9302fbe16843a729c9c6dfddc221df46d5d3f
Todd Mpeli <toddowenmpeli@rocketmail.com>
2024-02-20 15:14:13 +0000

    Updated mobile styling.

--- components/Faq.tsx
+++ components/Faq.tsx
@@ -62,7 +62,7 @@ const Faq: FC<IFaq> = ({title, subtitle, paragraph, faqGrid}) => {
 							tailwindStyling="lg:max-w-3xl mx-auto text-black text-base text-center"
 						/>
 					</motion.div>
-					<div className="w-full relative py-20 md:py-24 overflow-hidden">
+					<div className="w-full relative py-0 lg:py-20 lg:py-24 overflow-hidden">
 						<motion.div
 							initial={initial}
 							variants={stagger}
@@ -70,14 +70,14 @@ const Faq: FC<IFaq> = ({title, subtitle, paragraph, faqGrid}) => {
 							viewport={{once: true}}
 							className="max-w-7xl mx-auto flex flex-wrap -mx-4 -mb-8"
 						>
-							<div className="w-full lg:w-1/3 px-4 mb-15 lg:mb-0">
-								<div className="flex flex-wrap -mx-2 lg:flex-col lg:max-w-sm border-b lg:border-b-0 lg:border-r-2 border-blue-default">
-									<div className="w-full md:w-1/2 lg:w-full px-2 mb-15">
+							<div className="w-full lg:w-1/3 py-8 px-4 mb-15 lg:mb-0">
+								<div className="max-w-xl mx-auto lg:mx-0 lg:max-w-full flex flex-wrap -mx-2 lg:flex-col lg:max-w-sm border-b lg:border-b-0 lg:border-r-2 border-blue-default">
+									<div className="w-full lg:w-full px-2 mb-15">
 										<motion.h3
 											initial={initial}
 											whileInView={fadeInUp}
 											viewport={{once: true}}
-											className="uppercase text-black text-left font-extrabold text-lg mb-9"
+											className="uppercase text-black text-center sm:text-left font-extrabold text-lg mb-9"
 										>
 											Getting Started
 										</motion.h3>
@@ -86,6 +86,7 @@ const Faq: FC<IFaq> = ({title, subtitle, paragraph, faqGrid}) => {
 											variants={stagger}
 											whileInView="animate"
 											viewport={{once: true}}
+											className="flex flex-col sm:items-baseline items-center justify-center"
 										>
 											{faqGrid?.length > 0 ? (
 												faqGrid?.map((item: any, keys: number) => (
@@ -118,10 +119,10 @@ const Faq: FC<IFaq> = ({title, subtitle, paragraph, faqGrid}) => {
 																		cy="6"
 																		r="5"
 																		stroke="#C3C6CE"
-																		stroke-width="2"
+																		strokeWidth="2"
 																	></circle>
 																</svg>
-																<span className="ml-3 text-left">
+																<span className="ml-3 text-center sm:text-left">
 																	{item?.card?.title}
 																</span>
 															</button>
@@ -136,14 +137,14 @@ const Faq: FC<IFaq> = ({title, subtitle, paragraph, faqGrid}) => {
 								</div>
 							</div>
 							<div className="w-full lg:w-2/3 px-4">
-								<div className="max-w-xl xl:max-w-3xl mx-auto lg:mr-0">
+								<div className="max-w-xl xl:max-w-3xl mx-auto py-8 lg:py-0 lg:mr-0">
 									{selectedItemIndex !== null && (
 										<>
 											<motion.h3
 												initial={initial}
 												whileInView={fadeInUp}
 												viewport={{once: true}}
-												className="uppercase text-black text-left font-extrabold text-lg mb-6"
+												className="uppercase text-black text-center sm:text-left font-extrabold text-lg mb-6"
 											>
 												{faqGrid[selectedItemIndex]?.card?.title}
 											</motion.h3>
